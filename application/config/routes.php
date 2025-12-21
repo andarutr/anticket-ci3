1e423116f1ad6f6640d83a72302f3357f28e405c
andarutr <andarutr@gmail.com>
2025-12-21 18:14:33 +0700

    Create Monitoring Tickets User

--- application/config/routes.php
+++ application/config/routes.php
@@ -60,3 +60,4 @@ $route['user/ticket/request-system'] = 'user/ticket/requestsystem/index';
 $route['user/ticket/request-feature'] = 'user/ticket/requestfeature/index';
 $route['user/ticket/report-bug'] = 'user/ticket/reportbug/index';
 $route['user/ticket/request-meeting'] = 'user/ticket/requestmeeting/index';
+$route['user/ticket/monitoring-ticket'] = 'user/ticket/monitoringticket/index';
--- application/views/layouts/app/sidebar.php
+++ application/views/layouts/app/sidebar.php
@@ -27,6 +27,15 @@
 					</a>
 				</li>
 				<li class="nav-item">
+					<a class="nav-link" href="<?= site_url('/user/ticket/monitoring-ticket'); ?>">
+						<div
+							class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+							<i class="bi-ticket-perforated text-dark text-sm opacity-10"></i>
+						</div>
+						<span class="nav-link-text ms-1">Monitoring Ticket</span>
+					</a>
+				</li>
+				<li class="nav-item">
 					<a class="nav-link" href="<?= site_url('/user/ticket/request-system'); ?>">
 						<div
 							class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
@@ -60,6 +69,15 @@
 							<i class="bi-calendar-check text-dark text-sm opacity-10"></i>
 						</div>
 						<span class="nav-link-text ms-1">Request Meeting</span>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="<?= site_url('/user/ticket/close-ticket'); ?>">
+						<div
+							class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+							<i class="bi-ui-checks text-dark text-sm opacity-10"></i>
+						</div>
+						<span class="nav-link-text ms-1">Close Ticket</span>
 					</a>
 				</li>
 			<?php endif; ?>
