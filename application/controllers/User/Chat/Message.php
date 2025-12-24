34129fa8013e75f27092d03e347ed5208a1a9a51
andarutr <andarutr@gmail.com>
2025-12-24 11:18:20 +0700

    Create Feature Chat (FE)#2

--- application/controllers/User/Chat/Message.php
+++ application/controllers/User/Chat/Message.php
@@ -7,10 +7,36 @@ class Message extends CI_Controller {
         cek_login($this);
     }
 	
+    public function getChatDetail()
+    {
+        $id_ticket = $this->input->get('id');
+        
+        $query = "
+        SELECT * FROM chats 
+        WHERE ticket_id = ?
+        ";
+        $data = $this->db->query($query,[$id_ticket])->result();
+
+        echo json_encode($data);
+    }
+
 	public function index()
 	{
         $data['title'] = 'Chat Worker';
-        
+        $nik = $this->session->userdata('nik');
+
+        $query = "SELECT 
+            t.no_ticket,
+            t.developer_name,
+            t.updated_at,
+            s.id as id_ticket,
+            s.name as system_name
+        FROM tickets t
+        JOIN systems s ON t.system_id = s.id
+        WHERE t.requestor_nik = ? AND t.developer_name IS NOT NULL";
+
+        $data['tickets'] = $this->db->query($query, [$nik])->result();
+
 		$this->load->view('layouts/app/header', $data);
 		$this->load->view('layouts/app/sidebar');
 		$this->load->view('layouts/app/navbar');
--- assets/js/pages/user/message.js
+++ assets/js/pages/user/message.js
@@ -1,3 +1,46 @@
+function showMessageDetail(id_ticket, no_ticket, developer_name, system_name){
+    $.ajax({
+        type: "GET",
+        url: "/user/chat/message/getChatDetail",
+        data: {
+            id: id_ticket
+        },
+        success: function(res){
+            console.log(res);
+            $("#containerChatDetail").empty();
+            $("#containerChatDetail").append(`
+                <div id="detail-chat-1" class="chat-detail-content">
+                    <div class="chat-header mb-3">
+                        <h5 id="chat-ticket-title-1">&nbsp;&nbsp;&nbsp;#${no_ticket} - ${system_name}</h5>
+                    </div>
+                    <div class="chat-messages p-3 border rounded mb-3" style="height: 400px; overflow-y: auto;">
+                        <div class="message mb-2">
+                        <div class="d-flex justify-content-start">
+                            <div class="bg-primary text-white p-2 rounded">
+                            <small>Halo, saya ingin menanyakan tentang fitur absensi.</small><br>
+                            <small>User - 24/12/2025 10:05</small>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="message mb-2">
+                        <div class="d-flex justify-content-end">
+                            <div class="bg-light p-2 rounded">
+                            <small>Baik, fitur absensi saat ini sedang dalam pengembangan.</small><br>
+                            <small>Andaru Triadi - 24/12/2025 10:15</small>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="message-input-1" placeholder="Ketik pesan...">
+                        <button class="btn btn-primary" id="send-btn-1">Kirim</button>
+                    </div>
+                </div>
+            `);
+        }
+    })
+}
+
 $('.chat-item').on('click', function(e) {
     e.preventDefault();
 
@@ -8,4 +51,4 @@ $('.chat-item').on('click', function(e) {
     const ticketId = $(this).data('ticket-id');
 
     $(`#detail-chat-${ticketId}`).removeClass('d-none');
-  });
+});
