83fa44abed561ff9aba1a176be5a685c71dff9c9
andarutr <andarutr@gmail.com>
2025-12-24 09:54:18 +0700

    Improve UI Sidebar Show & Hide Sidebar

--- application/views/layouts/app/navbar.php
+++ application/views/layouts/app/navbar.php
@@ -1,7 +1,7 @@
 <main class="main-content position-relative border-radius-lg ">
-    <!-- Navbar -->
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
-      data-scroll="false">
+  <!-- Navbar -->
+  <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
+  data-scroll="false">
       <div class="container-fluid py-1 px-3">
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -21,6 +21,15 @@
               <a href="<?= site_url('auth/logout'); ?>" class="mt-3 btn btn-danger">
                 <i class="bi bi-arrow-bar-right me-sm-1"></i>
                 <span class="d-sm-inline d-none">LOGOUT</span>
+              </a>
+            </li>
+            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                <div class="sidenav-toggler-inner">
+                  <i class="sidenav-toggler-line bg-white"></i>
+                  <i class="sidenav-toggler-line bg-white"></i>
+                  <i class="sidenav-toggler-line bg-white"></i>
+                </div>
               </a>
             </li>
           </ul>
